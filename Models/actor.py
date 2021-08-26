9e5b60689ed53efea0778a773061445cddc7fbd2
ajayn1997 <the.ajay.great@gmail.com>
2021-08-26 10:15:39 +0530

    Added actor and critic architecture

---
+++ Models/actor.py
@@ -0,0 +1,154 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from base_models import Encoder, Pointer, Attention
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class DRL4TSP(nn.Module):
+    """Defines the main Encoder, Decoder, and Pointer combinatorial models.
+
+    Parameters
+    ----------
+    static_size: int
+        Defines how many features are in the static elements of the model
+        (e.g. 2 for (x, y) coordinates)
+    dynamic_size: int > 1
+        Defines how many features are in the dynamic elements of the model
+        (e.g. 2 for the VRP which has (load, demand) attributes. The TSP doesn't
+        have dynamic elements, but to ensure compatility with other optimization
+        problems, assume we just pass in a vector of zeros.
+    hidden_size: int
+        Defines the number of units in the hidden layer for all static, dynamic,
+        and decoder output units.
+    update_fn: function or None
+        If provided, this method is used to calculate how the input dynamic
+        elements are updated, and is called after each 'point' to the input element.
+    mask_fn: function or None
+        Allows us to specify which elements of the input sequence are allowed to
+        be selected. This is useful for speeding up training of the networks,
+        by providing a sort of 'rules' guidlines to the algorithm. If no mask
+        is provided, we terminate the search after a fixed number of iterations
+        to avoid tours that stretch forever
+    num_layers: int
+        Specifies the number of hidden layers to use in the decoder RNN
+    dropout: float
+        Defines the dropout rate for the decoder
+    """
+
+    def __init__(self, static_size, dynamic_size, hidden_size,
+                 update_fn=None, mask_fn=None, num_layers=1, dropout=0.):
+        super(DRL4TSP, self).__init__()
+
+        if dynamic_size < 1:
+            raise ValueError(':param dynamic_size: must be > 0, even if the '
+                             'problem has no dynamic elements')
+
+        self.update_fn = update_fn
+        self.mask_fn = mask_fn
+
+        # Define the encoder & decoder models
+        self.static_encoder = Encoder(static_size, hidden_size)
+        self.dynamic_encoder = Encoder(dynamic_size, hidden_size)
+        self.decoder = Encoder(static_size, hidden_size)
+        self.pointer = Pointer(hidden_size, num_layers, dropout)
+
+        for p in self.parameters():
+            if len(p.shape) > 1:
+                nn.init.xavier_uniform_(p)
+
+        # Used as a proxy initial state in the decoder when not specified
+        self.x0 = torch.zeros((1, static_size, 1), requires_grad=True, device=device)
+
+    def forward(self, static, dynamic, decoder_input=None, last_hh=None):
+        """
+        Parameters
+        ----------
+        static: Array of size (batch_size, feats, num_cities)
+            Defines the elements to consider as static. For the TSP, this could be
+            things like the (x, y) coordinates, which won't change
+        dynamic: Array of size (batch_size, feats, num_cities)
+            Defines the elements to consider as static. For the VRP, this can be
+            things like the (load, demand) of each city. If there are no dynamic
+            elements, this can be set to None
+        decoder_input: Array of size (batch_size, num_feats)
+            Defines the outputs for the decoder. Currently, we just use the
+            static elements (e.g. (x, y) coordinates), but this can technically
+            be other things as well
+        last_hh: Array of size (batch_size, num_hidden)
+            Defines the last hidden state for the RNN
+        """
+
+        batch_size, input_size, sequence_size = static.size()
+
+        if decoder_input is None:
+            decoder_input = self.x0.expand(batch_size, -1, -1)
+
+        # Always use a mask - if no function is provided, we don't update it
+        mask = torch.ones(batch_size, sequence_size, device=device)
+
+        # Structures for holding the output sequences
+        tour_idx, tour_logp = [], []
+        max_steps = sequence_size if self.mask_fn is None else 1000
+
+        # Static elements only need to be processed once, and can be used across
+        # all 'pointing' iterations. When / if the dynamic elements change,
+        # their representations will need to get calculated again.
+        static_hidden = self.static_encoder(static)
+        dynamic_hidden = self.dynamic_encoder(dynamic)
+
+        for _ in range(max_steps):
+
+            if not mask.byte().any():
+                break
+
+            # ... but compute a hidden rep for each element added to sequence
+            decoder_hidden = self.decoder(decoder_input)
+
+            probs, last_hh = self.pointer(static_hidden,
+                                          dynamic_hidden,
+                                          decoder_hidden, last_hh)
+            probs = F.softmax(probs + mask.log(), dim=1)
+
+            # When training, sample the next step according to its probability.
+            # During testing, we can take the greedy approach and choose highest
+            if self.training:
+                m = torch.distributions.Categorical(probs)
+
+                # Sometimes an issue with Categorical & sampling on GPU; See:
+                # https://github.com/pemami4911/neural-combinatorial-rl-pytorch/issues/5
+                ptr = m.sample()
+                while not torch.gather(mask, 1, ptr.data.unsqueeze(1)).byte().all():
+                    ptr = m.sample()
+                logp = m.log_prob(ptr)
+            else:
+                prob, ptr = torch.max(probs, 1)  # Greedy
+                logp = prob.log()
+
+            # After visiting a node update the dynamic representation
+            if self.update_fn is not None:
+                dynamic = self.update_fn(dynamic, ptr.data)
+                dynamic_hidden = self.dynamic_encoder(dynamic)
+
+                # Since we compute the VRP in minibatches, some tours may have
+                # number of stops. We force the vehicles to remain at the depot 
+                # in these cases, and logp := 0
+                is_done = dynamic[:, 1].sum(1).eq(0).float()
+                logp = logp * (1. - is_done)
+
+            # And update the mask so we don't re-visit if we don't need to
+            if self.mask_fn is not None:
+                mask = self.mask_fn(mask, dynamic, ptr.data).detach()
+
+            tour_logp.append(logp.unsqueeze(1))
+            tour_idx.append(ptr.data.unsqueeze(1))
+
+            decoder_input = torch.gather(static, 2,
+                                         ptr.view(-1, 1, 1)
+                                         .expand(-1, input_size, 1)).detach()
+
+        tour_idx = torch.cat(tour_idx, dim=1)  # (batch_size, seq_len)
+        tour_logp = torch.cat(tour_logp, dim=1)  # (batch_size, seq_len)
+
+        return tour_idx, tour_logp
--- Models/critc.py
+++ Models/critc.py
@@ -1,16 +1,67 @@
-import os
-import time
-import argparse
-import datetime
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torch.utils.data import DataLoader
+from base_models import Encoder
 
-from base_models import DRL4TSP, Encoder
-
-torch.backends.cudnn.benchmark = True
-torch.backends.cudnn.enabled=False
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class StateCritic(nn.Module):
+    """Estimates the problem complexity.
+
+    This is a basic module that just looks at the log-probabilities predicted by
+    the encoder + decoder, and returns an estimate of complexity
+    """
+
+    def __init__(self, static_size, dynamic_size, hidden_size):
+        super(StateCritic, self).__init__()
+
+        self.static_encoder = Encoder(static_size, hidden_size)
+        self.dynamic_encoder = Encoder(dynamic_size, hidden_size)
+
+        # Define the encoder & decoder models
+        self.fc1 = nn.Conv1d(hidden_size * 2, 20, kernel_size=1)
+        self.fc2 = nn.Conv1d(20, 20, kernel_size=1)
+        self.fc3 = nn.Conv1d(20, 1, kernel_size=1)
+
+        for p in self.parameters():
+            if len(p.shape) > 1:
+                nn.init.xavier_uniform_(p)
+
+    def forward(self, static, dynamic):
+
+        # Use the probabilities of visiting each
+        static_hidden = self.static_encoder(static)
+        dynamic_hidden = self.dynamic_encoder(dynamic)
+
+        hidden = torch.cat((static_hidden, dynamic_hidden), 1)
+
+        output = F.relu(self.fc1(hidden))
+        output = F.relu(self.fc2(output))
+        output = self.fc3(output).sum(dim=2)
+        return output
+
+class Critic(nn.Module):
+    """Estimates the problem complexity.
+
+    This is a basic module that just looks at the log-probabilities predicted by
+    the encoder + decoder, and returns an estimate of complexity
+    """
+
+    def __init__(self, hidden_size):
+        super(Critic, self).__init__()
+
+        # Define the encoder & decoder models
+        self.fc1 = nn.Conv1d(1, hidden_size, kernel_size=1)
+        self.fc2 = nn.Conv1d(hidden_size, 20, kernel_size=1)
+        self.fc3 = nn.Conv1d(20, 1, kernel_size=1)
+
+        for p in self.parameters():
+            if len(p.shape) > 1:
+                nn.init.xavier_uniform_(p)
+
+    def forward(self, input):
+
+        output = F.relu(self.fc1(input.unsqueeze(1)))
+        output = F.relu(self.fc2(output)).squeeze(2)
+        output = self.fc3(output).sum(dim=2)
+        return output
